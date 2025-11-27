9e1a89bee035ff4cc06eead55aa867196a692cd8
yugant-singh <yugantsingh9651@gmail.com>
2025-11-27 22:31:15 +0530

    add new video

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ addEventListener("mousemove", function(e){
    document.body.style.setProperty("--x", e.clientX+'px');
    document.body.style.setProperty("--y", e.clientY+'px');
 })
+window.addEventListener("click", () => {
+  const video = document.querySelector("video");
+  video.muted = false; 
+  video.play();
+});
